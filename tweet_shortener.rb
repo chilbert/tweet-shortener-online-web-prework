a08d966d65f3bf18b0f0c7e975d0be7b688caa9a
Chase Hilbert <chase@heyspacetime.com>
2019-02-07 18:26:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,12 +2,12 @@ def dictionary
 
   dictionary = {
     "hello" => 'hi',
-    "to, two, too"=> '2', 
+    "to, two, too"=> '2',
     "for, four"=> '4',
     'be' => 'b',
     'you' => 'u',
-    "at" => "@", 
+    "at" => "@",
     "and" => "&"
   }
-  
+
 end
