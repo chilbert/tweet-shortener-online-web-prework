05b2989e4ebbcc0244bf7d32f28d19dead3d68db
Chase Hilbert <chase@heyspacetime.com>
2019-02-07 18:26:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,8 +1,13 @@
-def selective_tweet_shortener(tweet)
+def dictionary
 
-tweet_size = tweet.size
-
-if tweet_size < 130
-  return tweet
-  end
- end
+  dictionary = {
+    "hello" => 'hi',
+    "to, two, too"=> '2', 
+    "for, four"=> '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@", 
+    "and" => "&"
+  }
+  
+end
